cb876aa74f6b44b0176cf02b648fc1bf05183d35
Tobias Hildebrandt <tobiash@tobiash.dev>
2021-09-21 21:06:13 -0400

    expand pitch size to 8 bits

--- src/include/note.h
+++ src/include/note.h
@@ -1,7 +1,7 @@
-#define no_note 31
-#define end_note -31
+#define no_note 127
+#define end_note -127
 #define invalid_note -9999
 struct Note {
-    signed char pitch:6; // half steps away from middle C, -31 to +31, see special cases
+    signed char pitch:8; // half steps away from middle C, -127 to +127, see special cases
     char length:4; // 0 = 1/32, 1 = 1/16, 2 = 1/8, 3 = 1/4, 4 = 1/2, 5 = 1, 6 = 2, 7 = 4, 8 = 8
 };
